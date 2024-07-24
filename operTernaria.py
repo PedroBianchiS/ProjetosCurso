1c0c482cd181dcf258257ff8b7710ac569a1640f
Bianchi <pedro.stumpf@yahoo.com.br>
2024-07-24 16:13:34 -0300

    primeira tentativa validarCpf
    código que fiz por conta próprio para validar um CPF qualquer

---
+++ operTernaria.py
@@ -0,0 +1,11 @@
+# <valor> if <condicao> else <outro_valor>
+
+print('valor' if True else 'Outro valor')
+
+condicao = 2 == 2
+variavel = 'primeiro' if condicao else 'segundo'
+print(variavel)
+
+digito = 10
+novoDigito = digito if digito <=9 else 0
+print(novoDigito)
---
+++ splitJoin.py
@@ -0,0 +1,8 @@
+frase = 'O rato roeu a roupa do rei de Roma'
+print(frase)
+
+listaPalavras = frase.split()
+print(listaPalavras)
+
+listaSeparada = '-'.join(frase)
+print(listaSeparada)
