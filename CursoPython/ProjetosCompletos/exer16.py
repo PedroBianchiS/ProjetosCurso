6ac25743260ddf12d8051d77ff361ca64ff24281
Bianchi <pedro.stumpf@yahoo.com.br>
2024-09-01 12:49:17 -0300

    Exer16 Corrigido!
    Adicionada verificação se valores formam triângulo válido

--- CursoPython/ProjetosCompletos/exer16.py
+++ CursoPython/ProjetosCompletos/exer16.py
@@ -1,9 +1,6 @@
 """ 16 - Faça um algoritmo que leia três valores que representam os três lados de um triângulo e verifique se são válidos, determine se o triângulo é equilátero, isósceles ou escaleno. """
 def verTriangulo(a, b, c):
-    if a + b < c or a + c < b or b + c < a:
-        return True
-    else:
-        return False
+    return a + b > c and a + c > b and b + c > a
 
 def tipoTriangulo(l1, l2, l3):
     if l1 == l2 == l3:
@@ -17,4 +14,8 @@ l1 = int(input('1º Lado: '))
 l2 = int(input('2º Lado: '))
 l3 = int(input('3º Lado: '))
 
-tipoTriangulo(l1, l2, l3)
+if verTriangulo(l1, l2, l3):
+    print('\nOs valores formam um triângulo válido!')
+    print(tipoTriangulo(l1, l2, l3))
+else:
+    print('Os valores não formam um triângulo válido!')
