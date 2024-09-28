822221e7e5a51c5120ec598ba54de91ab4de2bc5
Bianchi <pedro.stumpf@yahoo.com.br>
2024-09-28 15:31:43 -0300

    Update matrizRand.py

--- EstudosPython/matrizRand.py
+++ EstudosPython/matrizRand.py
@@ -1,2 +1,9 @@
-matrix = [[]]
+import numpy as np
+from random import randint
 
+i = int(input('Digite a altura da matriz: '))
+j = int(input('Digite a largura da matriz: '))
+
+matrizAleatória = np.random.randint(0,10, size=(i, j))
+
+print(matrizAleatória)
