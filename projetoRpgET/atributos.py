c9a460115b169cb8211358aaec16bc51dbd25e05
Bianchi <pedro.stumpf@yahoo.com.br>
2024-11-06 21:10:34 -0300

    Stuff

--- projetoRpgET/atributos.py
+++ projetoRpgET/atributos.py
@@ -1,26 +1,24 @@
 #atributos de personagem; físicos e mentais
 
 atributosFis = {
-    'força': 0,
-    'destreza': 0,
-    'percepção': 0,
-    'constituição': 0,
+    'Atributo1': 'Força', 'base': 0,
+    'Atributo2': 'Destreza', 'base': 0,
+    'Atributo3': 'Percepção', 'base': 0,
+    'Atributo4': 'Costituição', 'base': 0
 }
 atributosMen = {
     'presenca': 0,
     'labia': 0,
     'inteligencia': 0,
-    'espirito': 0,
+    'sabedoria': 0,
 }
 
 #Definição de Atributos
+pontAtr = [6, 5, 5, 4, 4, 3, 2]
+print(f'Posicione um dos numeros {pontAtr} entre os Atributos como desejar.')
 
-print('Posicione os numeros 6, 5, 5, 4, 4, 3, 3 e 2 entre os Atributos como desejar.')
-atributosFis['força'] = int(input('Força: '))
-print(atributosFis['força'])
-atributosFis['desrteza'] = int(input('Destreza: '))
-print(atributosFis['desrteza'])
-atributosFis['percepção'] = int(input('Percepção: '))
-print(atributosFis['percepção'])
-atributosFis['constituição'] = int(input('constituição: '))
-print(atributosFis['constituição'])
+def definirAtrFis():
+    for atr in atributosFis:
+        atributosFis['base'] = int(input(f'{atributosFis[atr]}: '))
+
+print(definirAtrFis())
